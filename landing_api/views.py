aa1ae6ae36d3ddd7afb742619984c8aefcf413b9
JosephMiranda87 <150175624+JosephMiranda87@users.noreply.github.com>
2026-01-07 19:42:25 +0000

    GUIA 21

--- landing_api/views.py
+++ landing_api/views.py
@@ -8,5 +8,26 @@ from datetime import datetime
 # Create your views here.
 class LandingAPI(APIView):
     name = "Landing API"
-    collection_name = "landing"
+    collection_name = "contactos"
+
+
+    def get(self, request):
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+      # get: Obtiene todos los elementos de la colección
+      data = ref.get()
+      # Devuelve un arreglo JSON
+      return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+      data = request.data
+      # Referencia a la colección
+      ref = db.reference(f'{self.collection_name}')
+      current_time  = datetime.now()
+      custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+      data.update({"timestamp": custom_format })
+      # push: Guarda el objeto en la colección
+      new_resource = ref.push(data)
+      # Devuelve el id del objeto guardado
+      return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
     
